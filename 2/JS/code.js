36068b46a616127b67bf282a9a000178133d4e48
crips24 <92549357+crips24@users.noreply.github.com>
2022-11-17 18:05:20 +0000

    s

--- 2/JS/code.js
+++ 2/JS/code.js
@@ -1,11 +1,12 @@
+
 const wrapper = document.getElementById("bubble-wrapper");
 
 const animateBubble = x => {
 const bubble = document.createElement("div");
 bubble.className = "bubble";
-bubble.style.left = '${x}px' ;
-wrapper.appendChilde(bubble);
-setTimeout(() => wrapper.removeChilde(bubble), 2000);
+bubble.style.left = '${x}px';
+wrapper.appendChild(bubble);
+setTimeout(() => wrapper.removeChild(bubble), 2000);
 }
 
 window.onmousemove = e => animateBubble(e.clientX);
